438609fd44835c1d7c67e5b8f5718376c74bc8a2
duanyf <675018796@qq.com>
2022-09-14 21:05:22 +0800

    first commit

---
+++ src/main/java/com/duanyf/git/GitTest.java
@@ -0,0 +1,14 @@
+package com.duanyf.git;
+
+/**
+ * @Description:
+ * @Author: duanyf
+ * @DateTime: 2022/9/14 0014 20:57
+ */
+public class GitTest {
+
+  public static void main(String[] args) {
+    System.out.println("hello git!");
+  }
+
+}
