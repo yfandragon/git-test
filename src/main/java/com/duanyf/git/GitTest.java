148357c86c4c3a99a66cb92341c153a3b5d42784
yfandragon <82740927+yfandragon@users.noreply.github.com>
2022-09-14 22:36:50 +0800

    pull commit

--- src/main/java/com/duanyf/git/GitTest.java
+++ src/main/java/com/duanyf/git/GitTest.java
@@ -14,6 +14,7 @@ public class GitTest {
     System.out.println("master");
     System.out.println("hot fix test");
     System.out.println("push fix test");
+    System.out.println("pull fix test");
   }
 
 }
